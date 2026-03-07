cc7a3f0aecd574ea4eab7d99a55da5d04715934d
RainyXeon <rainyxeon@gmail.com>
2026-03-07 17:43:59 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DeepLunaria Foundation official website
+
+All code, contents and media rights reversed to @RainyXeon
---
+++ index.js
@@ -0,0 +1,10 @@
+const rootCss = document.querySelector(':root')
+
+if (Math.random() < 0.5) {
+  rootCss.style.setProperty('--pastel-orange-blade', '#F2CB9D')
+  rootCss.style.setProperty('--pastel-purple-blade', '#D5B2D8')
+  rootCss.style.setProperty('--pastel-green-blade', '#BDF0C1')
+  rootCss.style.setProperty('--pastel-red-blade', '#F2A4A4')
+  rootCss.style.setProperty('--pastel-yellow-blade', '#F0F2AD')
+  rootCss.style.setProperty('--pastel-blue-blade', '#94EBF2')
+}
