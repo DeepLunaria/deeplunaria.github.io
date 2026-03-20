32314534602bc06c1540e4d579b50ea48dff3667
RainyXeon <rainyxeon@gmail.com>
2026-03-20 20:51:44 +0700

    move: /rainyxeon from /members

--- data/members_list.js
+++ data/members_list.js
@@ -3,6 +3,6 @@ export const members_list = [
     name: "RainyXeon (Founder)",
     description: "A full-stack developer from Vietnam.",
     color: "--pastel-blue-blade",
-    link: "/members/rainyxeon"
+    link: "/rainyxeon"
   },
 ]
